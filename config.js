b05120c5a0bc3798ed282b95fa29aff121b49053
dearvae <lovevae7@gmail.com>
2018-07-31 19:34:32 +0800

    ok

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 module.exports =  {
 	// serverURL: '25.37.100.106:8000'
-	// serverURL: 'localhost:8000',
-<<<<<<< HEAD
-	serverURL:'172.22.74.167:8000'
-=======
-	serverURL: '172.22.74.167:8000'
->>>>>>> 2d19697805d24e13e0c0b65a02f5b1d5fdd3f028
+	serverURL:'localhost:8000'
+	// serverURL: '172.22.74.167:8000'
 }
