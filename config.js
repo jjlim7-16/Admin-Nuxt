fa148836468c2dc7383234c346cf37c033de03f1
jjlim7-16 <jjlim7.16@ichat.sp.edu.sg>
2018-08-10 15:51:05 +0800

    change serverUrl

--- config.js
+++ config.js
@@ -2,14 +2,5 @@ module.exports = {
 	// hamachi
 	// serverURL: '25.37.100.106:8000'
 
-	// serverURL: 'localhost:8000'
-
-	// KidZania Server
-	hostname: '152.10.0.239'
-
-	// school server laptop
-	// serverURL: '172.22.74.167:8000'
-
-	// amazon
-	// serverURL: 'ec2-52-33-181-199.us-west-2.compute.amazonaws.com:8000'
+	serverURL: '152.10.0.239:8000'
 }
